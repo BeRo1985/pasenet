8ceb7007973d2872b21102f8b5244277f65f6291
Benjamin Rosseaux <benjamin@rosseaux.de>
2017-08-10 21:43:05 +0200

    - Bugfixes

--- examples/test1/test1.dpr
+++ examples/test1/test1.dpr
@@ -26,13 +26,17 @@ var address:TENetAddress;
     server,client:PENetHost;
     event:TENetEvent;
 begin
- address.host:=ENET_HOST_ANY;
+ if ParamCount>0 then begin
+  address.host:=ENET_HOST_ANY;
+ end else begin
+  enet_address_set_host(@address,PAnsiChar('127.0.0.1'));
+ end;
  address.port:=64242;
  server:=enet_host_create(@address,32,2,0,0);
  if assigned(server) then begin
   try
    client:=nil;
-   while (not Terminated) and (enet_host_service(server,@event,1000)>=0) do begin
+   while (not Terminated) and (enet_host_service(server,@event,3000)>=0) do begin
     case event.type_ of
      ENET_EVENT_TYPE_NONE:begin
       writeln('Server: Nothing');
@@ -60,36 +64,61 @@ var address:TENetAddress;
     server,client:PENetHost;
     event:TENetEvent;
     peer:PENetPeer;
+    Disconnected:boolean;
 begin
  client:=enet_host_create(nil,1,2,57600 shr 3,14400 shr 3);
  if assigned(client) then begin
   try
-   enet_address_set_host(@address,PAnsiChar(AnsiString(ParamStr(2))));
+   if ParamCount>1 then begin
+    enet_address_set_host(@address,PAnsiChar(AnsiString(ParamStr(2))));
+   end else begin
+    enet_address_set_host(@address,PAnsiChar('127.0.0.1'));
+   end;
    address.port:=64242;
    peer:=enet_host_connect(client,@address,2,0);
    if assigned(peer) then begin
-    if (enet_host_service(client,@event,5000)>=0) and
-       (event.type_=ENET_EVENT_TYPE_CONNECT) then begin
-     writeln('Connected');
-     while (not Terminated) and (enet_host_service(client,@event,1000)>=0) do begin
-      case event.type_ of
-       ENET_EVENT_TYPE_NONE:begin
-        writeln('Client: Nothing');
-       end;
-       ENET_EVENT_TYPE_CONNECT:begin
-        writeln('Client: Connected');
-       end;
-       ENET_EVENT_TYPE_DISCONNECT:begin
-        writeln('Client: Disconnected');
-       end;
-       ENET_EVENT_TYPE_RECEIVE:begin
-        writeln('Client: A packet received');
-        enet_packet_destroy(event.packet);
+    try
+     if (enet_host_service(client,@event,5000)>=0) and
+        (event.type_=ENET_EVENT_TYPE_CONNECT) then begin
+      writeln('Connected');
+      Disconnected:=false;
+      while (not Terminated) and (enet_host_service(client,@event,3000)>=0) do begin
+       case event.type_ of
+        ENET_EVENT_TYPE_NONE:begin
+         writeln('Client: Nothing');
+        end;
+        ENET_EVENT_TYPE_CONNECT:begin
+         writeln('Client: Connected');
+        end;
+        ENET_EVENT_TYPE_DISCONNECT:begin
+         writeln('Client: Disconnected');
+         Disconnected:=true;
+        end;
+        ENET_EVENT_TYPE_RECEIVE:begin
+         writeln('Client: A packet received');
+         enet_packet_destroy(event.packet);
+        end;
        end;
       end;
+      if not Disconnected then begin
+       writeln('Client: Disconnecting');
+       enet_peer_disconnect(peer,0);
+       while enet_host_service(client,@event,3000)>=0 do begin
+        case event.type_ of
+         ENET_EVENT_TYPE_RECEIVE:begin
+          enet_packet_destroy(event.packet);
+         end;
+         ENET_EVENT_TYPE_DISCONNECT:begin
+          writeln('Client: Disconnected');
+          break;
+         end;
+        end;
+       end;
+      end;
+     end else begin
+      writeln('Connection failed');
      end;
-    end else begin
-     writeln('Connection failed');
+    finally
      enet_peer_reset(peer);
     end;
    end;
@@ -123,6 +152,22 @@ begin
      Client.Terminate;
      Client.WaitFor;
      Client.Free;
+    end;
+   end else begin
+    Server:=TServer.Create(false);
+    try
+     Client:=TClient.Create(false);
+     try
+      readln;
+     finally
+      Client.Terminate;
+      Client.WaitFor;
+      Client.Free;
+     end;
+    finally
+     Server.Terminate;
+     Server.WaitFor;
+     Server.Free;
     end;
    end;
   finally
